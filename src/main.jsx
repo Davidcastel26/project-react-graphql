087ddc3a3cde34934cd81a2dad7e51130451aed9
Davidcastel26 <davcastellanoslarios@gmail.com>
2022-03-05 16:15:49 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,34 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-import {ApolloClient, gql} from '@apollo/client'
+import {ApolloClient, HttpLink, gql, InMemoryCache} from '@apollo/client'
 
-const client = new ApolloC
+const client = new ApolloClient({
+  // if we are not using the catche we will get an error 
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+   url: 'http://localhost:4000' 
+  })
+})
+
+const query =gql`
+  query{
+    allPersons{
+      id
+      name
+      phone
+      address{
+        street
+        city
+      }
+    }
+  }
+`
+
+client.query({query})
+  .then(res => {
+    console.log(res.data);
+  })
 
 ReactDOM.render(
   <React.StrictMode>
