1f2b39da7fb0e17bad3015580a435ea88719068b
Davidcastel26 <davcastellanoslarios@gmail.com>
2022-03-06 13:31:16 -0600

    Update PersonForm.jsx

--- src/components/PersonForm.jsx
+++ src/components/PersonForm.jsx
@@ -1,5 +1,16 @@
 import React from 'react'
 
+const CREATE_PERSON = gql`
+mutation createPersonHere ($name: String!, $street: String!, $city: String!){
+    addPerson(name: $name, street: $street, city: $city) {
+      name
+      address{
+        street
+        city
+      }
+    }
+  }
+`
 const PersonForm = () => {
   return (
     <div>PersonForm</div>
